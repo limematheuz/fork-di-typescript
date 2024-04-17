fb50765bea7c02cae803c6b995c9c313d9fc31c5
limematheuz <mribeiro17.info@gmail.com>
2024-04-17 15:23:19 +0200

    fisrt commit

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
 "use strict";
 class Book {
+    constructor(title) {
+        this.title = title;
+    }
+    read() {
+        return "Reading Book";
+    }
 }
-class PaperBook {
+class PaperBook extends Book {
+    constructor(title, pages, format) {
+        super(title);
+        this.pages = pages;
+        this.format = format;
+    }
+    read() {
+        return "Book in paper";
+    }
 }
-class ElectronicBook {
+class ElectronicBook extends Book {
+    constructor(title, size, pages) {
+        super(title);
+        this.size = size;
+        this.pages = pages;
+    }
+    read() {
+        return "Reading Book";
+    }
 }
-class ParchmentBook {
+class ParchmentBook extends Book {
+    constructor(title, length, size) {
+        super(title);
+        this.size = size;
+        this.length = length;
+    }
+    read() {
+        return "Reading Book";
+    }
 }
 class ReadBook {
+    constructor(books) {
+        this.books = books;
+    }
+    read() {
+        return this.books.read();
+    }
 }
 const phisicalBook = new PaperBook("Dragones de papel", 350);
 const digitalBook = new ElectronicBook("One piece", 150);
--- app.ts
+++ app.ts
@@ -1,18 +1,70 @@
-class Book{}
+class Book {
+  title: string;
 
-class PaperBook{}
+  constructor(title: string) {
+    this.title = title;
+  }
+  read() {
+    return "Reading Book";
+  }
+}
 
-class ElectronicBook{}
+class PaperBook extends Book {
+  pages: number;
+  format: string;
 
-class ParchmentBook{}
+  constructor(title: string, pages: number, format: string) {
+    super(title);
+    this.pages = pages;
+    this.format = format;
+  }
+  read() {
+    return "Book in paper";
+  }
+}
 
-class ReadBook{}
+class ElectronicBook extends Book {
+  size: number;
+  pages: number;
 
+  constructor(title: string, size: number, pages: number) {
+    super(title);
+    this.size = size;
+    this.pages = pages;
+  }
+  read() {
+    return "Reading Book";
+  }
+}
 
-const phisicalBook = new PaperBook("Dragones de papel",350);
-const digitalBook = new ElectronicBook("One piece",150);
+class ParchmentBook extends Book {
+  length: number;
+  size: number;
+
+  constructor(title: string, length: number, size: number) {
+    super(title);
+    this.size = size;
+    this.length = length;
+  }
+  read() {
+    return "Reading Book";
+  }
+}
+
+class ReadBook {
+  books:Book;
+  constructor(books:Book) {
+    this.books = books;
+  }
+  read() {
+    return this.books.read();
+  }
+}
+
+const phisicalBook = new PaperBook("Dragones de papel", 350);
+const digitalBook = new ElectronicBook("One piece", 150);
 const readerPaper = new ReadBook(phisicalBook);
-const readerDigital= new ReadBook(digitalBook);
+const readerDigital = new ReadBook(digitalBook);
 
-console.log (readerDigital.read());
+console.log(readerDigital.read());
 console.log(readerPaper.read());
